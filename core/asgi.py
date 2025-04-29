10c4e8202f6192135a9acd3f8c7b94288d39b500
kingarturvitorio <kingarturvitorio@gmail.com>
2025-04-29 00:26:09 -0300

    corrigir ordem de import em asgi.py

--- core/asgi.py
+++ core/asgi.py
@@ -1,16 +1,21 @@
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+from channels.routing import ProtocolTypeRouter, URLRouter
+
 import django
 from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+
+
 
 import esp32mqtt.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
 django.setup()
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             esp32mqtt.routing.websocket_urlpatterns
