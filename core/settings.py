c6c22803c6725ddf978e165cc6386509a9366aba
kingarturvitorio <kingarturvitorio@gmail.com>
2025-04-29 00:59:49 -0300

    allowed_host

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u&4mrfs9sl+6@7^f!c4qtrsgnb=gtf)d%pvxfr1d2)qk*yq2*$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [*]
 
 import os
 # Application definition
